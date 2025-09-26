724246eabf964c117772f9b07d3c1e2dd40d1602
hadis_soleymani <hadiissoleymani@gmail.com>
2025-09-26 09:59:20 -0700

    add styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,38 @@
-import { Button, StyleSheet, Text,TextInput,View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-     <View style={styles.appContainer}>
-     <View style={styles.textInputContainer}>
-      <TextInput style={styles.textInput}/>
-      <Button title='Add'/>
-     </View>
-     <View>
-      <Text>Goals list...</Text>
-     </View>
-     </View>        
+    <View style={styles.appContainer}>
+      <View style={styles.textInputContainer}>
+        <TextInput placeholder="add a goal" style={styles.textInput} />
+        <Button title="Add Goals" />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>Goals list...</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
-    padding:50,
+  appContainer: {
+    padding: 40,
+    flex: 1,
   },
-  textInputContainer:{
-   flexDirection:'row'
+  textInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 16,
   },
-  textInput:{
-    borderColor:'#aaa',
-    borderWidth:1,
-    width:300
-  }
+  textInput: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    width: "70%",
+  },
+  listContainer: {
+    flex: 3,
+    borderTopWidth: 1,
+    borderColor: "#ccc",
+    paddingTop: 16,
+  },
 });
